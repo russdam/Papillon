1e334e6c5a246aa569332274aa057969abbeec21
Russell Damerell-Moss <russell.damerell@me.com>
2024-11-13 11:51:58 +0200

    fill in license identifiers to gpl3

--- src/i18n.rs
+++ src/i18n.rs
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: {{LICENSE}}
+// SPDX-License-Identifier: GPL-3.0-or-later
 
 //! Provides localization support for this crate.
 
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: {{LICENSE}}
+// SPDX-License-Identifier: GPL-3.0-or-later
 
 mod app;
 mod config;
