86f2cc98296ec6c170faf5eccd008e76d9cd34eb
Russell Damerell-Moss <russell.damerell@me.com>
2024-11-14 05:55:44 +0200

    add copyright header to existing source files

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,20 @@
+// Copyright (C) 2024 Russell Damerell-Moss <russell.damerell@me.com>
 // SPDX-License-Identifier: GPL-3.0-or-later
+//
+// This file is part of CrossTalk.
+//
+// CrossTalk is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// CrossTalk is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with CrossTalk. If not, see <https://www.gnu.org/licenses/>.
 
 mod app;
 mod config;
